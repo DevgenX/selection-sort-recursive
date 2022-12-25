a3333c0c98876ac04d7c9b5aaebfb1586739e4ce
devgenx <sebastian110420@gmail.com>
2022-12-24 21:31:47 -0500

    finished

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,6 +1,28 @@
-function selectionSortRecursive(arr) {
-  // type your code here
-}
+const selectionSortRecursive = (arr) => {
+  // check if the arr still has length, if no return empty array
+  if (arr.length === 0) {
+    return arr;
+  }
+  //find the minium num from the array
+  const minNum = Math.min(...arr);
+  // console.log(minNum)
+  //get the index of that minimum number in the array
+  const index = arr.indexOf(minNum);
+
+  // splice the array starting from that number
+  arr.splice(index, 1);
+
+  // recurse the function , save the result into the result array
+  const resultArr = selectionSortRecursive(arr);
+  // console.log(result)
+  resultArr.unshift(minNum);
+  //console.log(resultArr)
+  return resultArr;
+};
+
+const array = [3, -1, 5, 2];
+
+console.log(selectionSortRecursive(array)); // should log : [-1, 2,3,5]
 
 if (require.main === module) {
   // add your own tests in here
